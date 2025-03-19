a274bedd79d5ee59c77e6af1d6073121ed3d9672
Remi Etaix Bonnin <remi.etaix.bonnin@gmail.com>
2025-03-19 12:09:20 +0100

    Search bar is working

--- app/controllers/cellars_controller.rb
+++ app/controllers/cellars_controller.rb
@@ -1,5 +1,8 @@
 class CellarsController < ApplicationController
   def show
     @cellar = current_user.cellar
+    @bottles = current_user.bottles
+    sql_subquery = "bottles.name ILIKE :query OR bottles.appellation ILIKE :query OR bottles.variety ILIKE :query"
+    @bottles = @bottles.where(sql_subquery, query: "%#{params[:query]}%") if params[:query].present?
   end
 end
